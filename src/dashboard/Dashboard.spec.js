8c45ac71619d1572e5846ba36b8559a8d4e5b5f5
AbdelI <homeworkoracle@protonmail.com>
2019-12-29 21:56:35 +0100

    First snapshot test is a success

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,21 @@
 // Test away
+import React from "react";
+import ReactDOM from "react-dom";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Dashboard from "./Dashboard";
+
+afterEach(rtl.cleanup);
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper = rtl.render(<Dashboard />);
+});
+
+it("renders without crashing", () => {
+  //   const div = document.createElement("div");
+  //   ReactDOM.render(<Dashboard />, div);
+  //   ReactDOM.unmountComponentAtNode(div);
+  expect(wrapper.container).toMatchSnapshot();
+});
