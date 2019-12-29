5b09e1c3f69ea607c74e8bf6393da746374808e7
AbdelI <homeworkoracle@protonmail.com>
2019-12-29 22:11:50 +0100

    test for unlocked working

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -9,6 +9,11 @@ afterEach(rtl.cleanup);
 
 let wrapper;
 
+let Unlocked = () => wrapper.queryByText("Unlocked");
+let Open = () => wrapper.queryByText("Open");
+let Lock = () => wrapper.queryByText("Lock Gate");
+let Close = () => wrapper.queryByText("Close Gate");
+
 beforeEach(() => {
   wrapper = rtl.render(<Dashboard />);
 });
@@ -18,4 +23,11 @@ it("renders without crashing", () => {
   //   ReactDOM.render(<Dashboard />, div);
   //   ReactDOM.unmountComponentAtNode(div);
   expect(wrapper.container).toMatchSnapshot();
+});
+
+it("renders a 'Unlocked' text node ", () => {
+  //   expect(wrapper.queryByText("Unlocked")).toBeInTheDocument();
+  //   expect(wrapper.queryByText("Unlocked")).toBeVisible();
+  expect(Unlocked()).toBeInTheDocument();
+  expect(Unlocked()).toBeVisible();
 });
