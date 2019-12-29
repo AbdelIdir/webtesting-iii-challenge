defb3aa17ce0a2857a82af8c44fe599766b098d9
AbdelI <homeworkoracle@protonmail.com>
2019-12-29 22:40:40 +0100

    All tests for display,when Closed or Open work properly

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -11,32 +11,55 @@ let wrapper;
 
 let Unlocked = () => wrapper.queryByText("Unlocked");
 let Open = () => wrapper.queryByText("Open");
-let Lock = () => wrapper.queryByText("Lock Gate");
-let Close = () => wrapper.queryByText("Close Gate");
+let LockGate = () => wrapper.queryByText("Lock Gate");
+let CloseGate = () => wrapper.queryByText("Close Gate");
+let Closed = () => wrapper.queryByText("Closed");
+let Locked = () => wrapper.queryByText("Locked");
 
 beforeEach(() => {
   wrapper = rtl.render(<Dashboard />);
 });
 
-it("renders without crashing", () => {
-  //   const div = document.createElement("div");
-  //   ReactDOM.render(<Dashboard />, div);
-  //   ReactDOM.unmountComponentAtNode(div);
-  expect(wrapper.container).toMatchSnapshot();
+describe("Dashboard component renders properly", () => {
+  it("renders without crashing", () => {
+    //   const div = document.createElement("div");
+    //   ReactDOM.render(<Dashboard />, div);
+    //   ReactDOM.unmountComponentAtNode(div);
+    expect(wrapper.container).toMatchSnapshot();
+  });
+
+  it("renders a 'Unlocked,Open,Lock Gate,Close Gate' text nodes ", () => {
+    //   expect(wrapper.queryByText("Unlocked")).toBeInTheDocument();
+    //   expect(wrapper.queryByText("Unlocked")).toBeVisible();
+    expect(Unlocked()).toBeInTheDocument();
+    expect(Unlocked()).toBeVisible();
+
+    expect(Open()).toBeInTheDocument();
+    expect(Open()).toBeVisible();
+
+    expect(LockGate()).toBeInTheDocument();
+    expect(LockGate()).toBeVisible();
+    expect(LockGate()).toBeDisabled();
+
+    expect(CloseGate()).toBeInTheDocument();
+    expect(CloseGate()).toBeVisible();
+
+    //   expect(Closed()).toBeInTheDocument();
+    //   expect(Closed()).toBeVisible();
+
+    //   expect(Locked()).toBeInTheDocument();
+    //   expect(Locked()).toBeVisible();
+  });
 });
 
-it("renders a 'Unlocked,Open,Lock Gate,Close Gate' text nodes ", () => {
-  //   expect(wrapper.queryByText("Unlocked")).toBeInTheDocument();
-  //   expect(wrapper.queryByText("Unlocked")).toBeVisible();
-  expect(Unlocked()).toBeInTheDocument();
-  expect(Unlocked()).toBeVisible();
+describe("Dashboard component,when we close the gate", () => {
+  it("renders Closed Gate text node properly when we click the button", () => {
+    rtl.fireEvent.click(CloseGate());
+    expect(CloseGate()).toBe(null);
+  });
 
-  expect(Open()).toBeInTheDocument();
-  expect(Open()).toBeVisible();
-
-  expect(Lock()).toBeInTheDocument();
-  expect(Lock()).toBeVisible();
-
-  expect(Close()).toBeInTheDocument();
-  expect(Close()).toBeVisible();
+  it(' shows that "Open" disappears when "Close Gate" is clicked ', () => {
+    rtl.fireEvent.click(CloseGate());
+    expect(Open()).toBe(null);
+  });
 });
